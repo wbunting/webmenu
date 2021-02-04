07a4fc8fe5662e611c0f6a24e6aa794dcddcaed1
wbunting <will.bunting@protonmail.com>
2021-02-04 23:40:13 +0900

    try taring something

---
+++ .github/actions/release/index.js
@@ -0,0 +1,28 @@
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+function execCommand(command, { cwd }) {
+  console.log(`running ${command}`);
+  const [cmd, ...args] = command.split(" ");
+  return execa(cmd, args, {
+    cwd,
+    shell: process.env.shell || true,
+    windowsHide: true,
+    stdio: "inherit",
+    env: { FORCE_COLOR: "0" },
+  }).then();
+}
+
+try {
+  // test
+  execCommand("touch test.txt && echo 'hi' >> test.txt", {
+    cwd: process.cwd(),
+  });
+
+  // tar the binary
+  execCommand("tar -czf test.txt.tgz test.txt", { cwd: process.cwd() });
+
+  // upload the binary to the release
+} catch (error) {
+  core.setFailed(error.message);
+}
