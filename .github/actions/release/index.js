7f54a1732d526043f314112c7e32f08c00488e9a
wbunting <will.bunting@protonmail.com>
2021-02-05 00:22:32 +0900

    execa

--- .github/actions/release/index.js
+++ .github/actions/release/index.js
@@ -1,5 +1,6 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
+import execa from "execa";
 
 function execCommand(command, { cwd }) {
   console.log(`running ${command}`);
