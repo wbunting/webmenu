475df5914d5962a56957f48a607ab9f478c0f9c9
Will Bunting <will.bunting@protonmail.com>
2021-02-07 09:14:23 +0000

    feat: debug ci (#44)

--- .github/actions/release/index.js
+++ .github/actions/release/index.js
@@ -3,7 +3,7 @@ const { getOctokit, context } = require("@actions/github");
 const execa = require("execa");
 const fs = require("fs");
 
-function execCommand(command, { cwd }) {
+async function execCommand(command, { cwd }) {
   console.log(`running ${command}`);
   const [cmd, ...args] = command.split(" ");
   return execa(cmd, args, {
@@ -12,44 +12,48 @@ function execCommand(command, { cwd }) {
     windowsHide: true,
     stdio: "inherit",
     env: { FORCE_COLOR: "0" },
-  }).then();
+  });
 }
 
-try {
-  const octokit = getOctokit({ auth: process.env.GITHUB_TOKEN });
-  // get the release number somehow
-  let releaseVersion = core.getInput("tag_name");
-  console.log("releaseVersion", releaseVersion);
+const main = async () => {
+  try {
+    const octokit = getOctokit({ auth: process.env.GITHUB_TOKEN });
+    // get the release number somehow
+    let releaseVersion = core.getInput("tag_name");
+    console.log("releaseVersion", releaseVersion);
 
-  // tar the binary
-  execCommand(
-    // `tar -czf webmenu_${releaseVersion}_x64.app.tgz ./src-tauri/target/release/webmenu`,
-    `tar -czf webmenu_${releaseVersion}_x64.app.tgz ./README.md`,
-    {
-      cwd: process.cwd(),
-    }
-  );
+    // tar the binary
+    await execCommand(
+      // `tar -czf webmenu_${releaseVersion}_x64.app.tgz ./src-tauri/target/release/webmenu`,
+      `tar -czf webmenu_${releaseVersion}_x64.app.tgz ./README.md`,
+      {
+        cwd: process.cwd(),
+      }
+    );
 
-  const release = octokit.repos.getReleaseByTag({
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-    tag: releaseVersion,
-  });
+    const release = await octokit.repos.getReleaseByTag({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      tag: releaseVersion,
+    });
 
-  console.log("release", release);
+    console.log("release", release);
 
-  execCommand("ls -a", { cwd: process.cwd() });
-  execCommand("pwd", { cwd: process.cwd() });
+    await execCommand("ls -a", { cwd: process.cwd() });
+    await execCommand("pwd", { cwd: process.cwd() });
 
-  octokit.repos.uploadReleaseAsset({
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-    release_id: release.id,
-    name: `webmenu_${releaseVersion}_x64.app.tgz`,
-    data: fs.readFileSync(
-      `${process.cwd()}/webmenu_${releaseVersion}_x64.app.tgz`
-    ),
-  });
-} catch (error) {
-  core.setFailed(error.message);
-}
+    await octokit.repos.uploadReleaseAsset({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      release_id: release.id,
+      name: `webmenu_${releaseVersion}_x64.app.tgz`,
+      data: fs.readFileSync(
+        `${process.cwd()}/webmenu_${releaseVersion}_x64.app.tgz`
+      ),
+    });
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+};
+
+main();
